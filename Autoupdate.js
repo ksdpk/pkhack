42f4d3e5285ddf4d8c1b393c5f0c7474dc01fefc
ksdpk <65320800+ksdpk@users.noreply.github.com>
2025-08-15 10:13:04 +0700

    Update Autoupdate.js
    เปิดใช้งาน Auto Fast Attack Click/Pokemon

--- Autoupdate.js
+++ Autoupdate.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const SCRIPT_NAME = "Pokéclicker Helper";
-    const VERSION = "1.6.0"; // เพิ่มเรทการกลายพันธุ์เบอรี่
+    const VERSION = "1.6.1"; // เปิดใช้งาน Auto Fast Attack Click/Pokemon
 
     const CONTAINER_ID = "poke-helper-container";
     let gameReady = false;
@@ -353,8 +353,8 @@
         [7,10,11].forEach(i => { App.game.oakItems.itemList[i].bonusList = [999999,999999,999999,999999,999999,999999]; App.game.oakItems.itemList[i].inactiveBonus = 999999; });
         BerryMutations.mutationChance = 100;
 
-        if (acOn) setAutoClick(true);
-        if (paOn) setFastPokemonAttack(true);
+        setAutoClick(true);
+        setFastPokemonAttack(true);
 
         if (typeof BattleFrontierBattle !== 'undefined') {
             BattleFrontierBattle._origPokemonAttack = BattleFrontierBattle.pokemonAttack;
